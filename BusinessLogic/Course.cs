0f98d6326c2b961dfe0a09c9abf0c03df9e42bb6
Dennis Kafton <boom1axe@att.net>
2019-11-04 16:32:40 -0800

    Course now passes tests

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,9 +6,14 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private int maxCredits = 30;
+        private string courseName;
+        private byte numberOfCredits;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = "STAFF";
         }
 
         /// <summary>
@@ -19,12 +24,34 @@ namespace BusinessLogic
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName
+        {
+            get => courseName;
+            set
+            {
+                if (value is null)
+                    throw new ArgumentNullException("CouseName is null");
+
+                courseName = value;
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
-        /// </summary>
-        public byte NumberOfCredits { get; set; }
+        /// </summary>     
+		public byte NumberOfCredits
+        {
+            get => numberOfCredits;
+            set
+            {
+                if (value == maxCredits || value > maxCredits)
+                    throw new ArgumentException("Credits over max value");
+                if (value == 0)
+                    throw new ArgumentException("Credits zero value");
 
+                numberOfCredits = value;
+            }
+        }
     }
+
 }
